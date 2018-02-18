1a5a3c7656d1e82caae35b2647a75b0a66577d60
Enrico <enricosaccon96@gmail.com>
2018-02-18 14:29:34 +0100

    Modify Arduino file to send digital result

--- distance_d/distance_d.ino
+++ distance_d/distance_d.ino
@@ -1,47 +1,85 @@
-//HC RS04 Sensore ultrasuoni
-//Giuseppe Caccavale
-const int triggerPort = 9;
-const int echoPort = 10;
-const int led = 13;
+#define triggerPort 9
+#define echoPort 10
+#define SEND 12
+#define ACTIVATE 11
+#define BIT 16
+
+int bin[BIT];
+
+void convert(double dist){
+	for (int i=0; i<BIT; i++){ //Reset binary
+		bin[i]=0;
+	}
+	dist*=100; 
+	long dist_l=(long) dist; //Tronco e converto in long
+	int i=0;
+	while(dist_l>1 && i<BIT){
+		bin[i]=dist_l%2;
+		dist_l=(long) (dist_l/2);
+		i++;
+	}
+	if (i<BIT){ //Controlla anche l'ultimo carattere
+		if (dist_l==1){
+			bin[i]=1;
+		}
+		else{
+			bin[i]=0;
+		}
+		for (int a=BIT-1; a>=0; a++){
+			Serial.print(bin[a]);
+		}
+		Serial.print("\n");
+	}
+	else{
+		for (int a=0; a<BIT; a++){ //QUESTO SIGNIFICA ERRORE
+			bin[a]=0;
+		}
+	}
+}
+
+void send(int* bin){
+	digitalWrite(ACTIVATE, HIGH); //Setto pin attivo per dire che sto per mandare segnale
+	delay(500); //Aspetto 1 millisecondo
+	for (int i=0; i<BIT; i++){
+		digitalWrite(SEND, (bin[i]==1 ? HIGH : LOW)); //Invio bit
+		delay(500);
+	}
+	digitalWrite(ACTIVATE, LOW); //Chiudo
+}
 
 void setup() {
 	pinMode(triggerPort, OUTPUT);
 	pinMode(echoPort, INPUT);
-	pinMode(led, OUTPUT);
+	pinMode(SEND, OUTPUT);
+	pinMode(ACTIVATE, OUTPUT);
 	Serial.begin(9600);
-	Serial.print( "Sensore Ultrasuoni: ");
 }
 
 void loop() {
 	//porta bassa l'uscita del trigger
-	digitalWrite( triggerPort, LOW );
+	digitalWrite(triggerPort, LOW);
 	//invia un impulso di 10microsec su trigger
-	digitalWrite( triggerPort, HIGH );
-	delayMicroseconds( 10 );
-	digitalWrite( triggerPort, LOW );
+	digitalWrite(triggerPort, HIGH);
+	delayMicroseconds(10);
+	digitalWrite(triggerPort, LOW);
 
-	long durata = pulseIn( echoPort, HIGH );
+	double length = pulseIn(echoPort, HIGH);
 
-	long distanza = 0.034 * durata / 2;
+	double distance = 0.034 * length / 2;
 
-	Serial.print("distanza: ");
+	Serial.print("distance: ");
 
 	//dopo 38ms è fuori dalla portata del sensore
-	if( durata > 38000 ){
-		Serial.println("Fuori portata   ");
+	if( length > 38000 ){
+		Serial.println("out of range");
 	}
 	else{ 
-		Serial.print(distanza); 
-		Serial.println(" cm     ");
+		Serial.print(distance); 
+		Serial.println(" cm");
 	}
+	
+	send(convert(distance));
 
-	if(distanza < 10){
-		digitalWrite(led, HIGH);
-	}
-	else{
-		digitalWrite(led, LOW);
-	}
-
-	//Aspetta 1000 microsecondi
+	//Aspetta 1000 millisecondi
 	delay(1000);
 }
