bc4233f6602095e12656abc338f1b111fbc4f1f2
icosac <enricosaccon96@gmail.com>
2018-03-30 14:05:27 +0000

    c'è temporale

--- i4c_d/i4c_d.ino
+++ i4c_d/i4c_d.ino
@@ -1,3 +1,9 @@
+// Since i2c sends one byte per time, I'll send 2 bytes. This are the error codes:
+// If distance is:
+// >500 => 502 send byte1=0xFF=255, byte2=0xF7=247 
+// <=0 => 503 send byte1=0xFF=255, byte2=0xF7=248
+// If signal was not recognized: 504 send byte1=0xFF=255, byte2=0xF7=249 
+
 #include <Wire.h>
 
 #define SLAVE_ADDRESS 0x04 // This is the address on which I'll be talking with Pi
@@ -19,7 +25,13 @@ void setup() {
 
 void loop() {
 	delay(1000);
-	distance=random(-2, 11);
+	distance=random(-2, 3);
+	if (distance==-2){
+		distance=550;
+	}
+	else if (distance==0){
+		distance==400;
+	}
 	Serial.print("distance: ");Serial.println(distance);
 }
 
@@ -27,18 +39,34 @@ void loop() {
 void receiveData(int byteCount){
 	while(Wire.available()) {
 		signal = Wire.read(); 
-		Serial.print("Data received: ");
-		Serial.println(signal);
+		Serial.print("Signal received: "); Serial.println(signal);
 	}
 }
 
 // I'll send data to my friend Pi
+// See top of document for information regarding error codes.
 void sendData(){
-	if (signal > 0){
-		Wire.write(distance);
-		Serial.print("Sent: ");Serial.print(distance);Serial.print(" for request: ");Serial.println(signal);
+	byte byte1, byte2;
+	if (signal > 0){ //Change with ==1
+		if (distance>500){
+			byte1=0xff; byte2=0xf7; //255+247=502
+		}
+		else if (distance<=0){
+			byte1=0xff; byte2=0xf8; //255+248=503
+		}
+		else if (distance>255 && distance <=500){
+			byte1=0xff; byte2=(byte)distance-0xff;
+		}
+		else {
+			byte1=(byte)distance; byte2=0x00;
+		}
+		Serial.print("Sent: ");Serial.print(byte1);Serial.print("; ");Serial.println(byte2);
 	}
 	else {
+		byte1=0xff; byte2=0xf9;
 		Serial.print("Got");Serial.print(signal);Serial.println(", I don't recognize this value");
 	}
+	byte buffer[2]={byte1, byte2};
+	Wire.write(buffer, 2);
+	Serial.print("Sent: ");Serial.print(byte1);Serial.print("; ");Serial.println(byte2);
 }
--- main.py
+++ main.py
@@ -1,34 +1,37 @@
 import RPi.GPIO as GPIO
-import smbus
+from smbus2 import SMBus, i2c_msg
 import time
 
 # for RPI version 0, use “bus = smbus.SMBus(0)”
-bus = smbus.SMBus(1)
+bus = SMBus(1)
 
 # This is the address we setup in the Arduino Program
 address = 0x04
 
 # Send integer value to arduino
 def send(value):
-	bus.write_byte(address, value)
-	# bus.write_byte_data(address, 0, value)
+	msg=None
+	msg=i2c_msg.write(address, [value])
+	bus.i2c_rdwr(msg)
 	return -1
 
 # Receive integer value from arduino
 def receive():
-	number = bus.read_byte(address)
-	# number = bus.read_byte_data(address, 1)
-	return number
+	msg = i2c_msg.read(address, 2)
+	bus.i2c_rdwr(msg)
+	number=0
+	for value in msg:
+		number+=value
+	return value
 
 # Send a signal to Arudino to recive a value
 # 1'll be the integer to tell arduino to send back a value
 def GetDistanceFromArd():
-	count=1
 	while True:
-		send(count)
-		print(str(count)+"Hey Arduino, I'm asking you for a distance value")
-		number=receive()
-		if (number<0): # If the value I get is less then 0 then there was an error
+		send(1) #1 is signal to request informations
+		print("Hey Arduino, I'm asking you for a distance value")
+		distance=receive()
+		if (distance==504): # If the value I get is less then 0 then there was an error
 			print("What's up Arudino, are you fine?")
 		else: 
 			print(str(count)+"Thanks, I received: "+str(number))
@@ -38,4 +41,15 @@ def GetDistanceFromArd():
 if __name__=="__main__":
 	GetDistanceFromArd()
 
-	
+# def GetDistanceFromArd():
+# 	count=1
+# 	while True:
+# 		send(count)
+# 		print(str(count)+"Hey Arduino, I'm asking you for a distance value")
+# 		number=receive()
+# 		if (number[0]<0): # If the value I get is less then 0 then there was an error
+# 			print("What's up Arudino, are you fine?")
+# 		else: 
+# 			print(str(count)+"Thanks, I received: "+str(number))
+# 		count+=1;
+# 		time.sleep(2)
